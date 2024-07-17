803fd5f37a11aadaa4b6824a7b72709de6ba1a2a
AFRIN ASHAR <98964571+afrinashar@users.noreply.github.com>
2024-07-17 11:35:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 /* Middleware */
 app.use(
   cors({
-    origin: "https://family-gallery-ten.vercel.app",
+    origin: "https://family-gallery-ten.vercel.app/",
   })
 );
 app.use(express.json());
